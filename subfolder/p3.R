b72dc50e0b1d3311b42e3a664e67894451380be0
ltbrenner <153870418+ltbrenner@users.noreply.github.com>
2024-05-18 11:14:03 -0700

    Delete subfolder directory

--- subfolder/p3.R
+++
@@ -1,6 +0,0 @@
-random_values_1 <- runif(100, min = 0, max = 1)
-random_values_2 <- runif(100, min = 0, max = 1)
-
-plot(random_values_1, random_values_2)
-lines(lowess(random_values_1, random_values_2))
-title("Scatterplot")
