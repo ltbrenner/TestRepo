171ea3d53dca85f3d1f6ee3a3fe02631f9495920
ltbrenner <153870418+ltbrenner@users.noreply.github.com>
2024-05-24 11:27:25 -0700

    Automatic commit generated by codereview web-app.

--- p1.R
+++ p1.R
@@ -2,16 +2,12 @@ library(carData)
 data("Guyer")
 str(Guyer)
 
-mean_coop = mean(Guyer$cooperation)
-print(paste("Cooperation Mean", mean_coop))
+average_coop = mean(Guyer$cooperation)
+print(paste("Cooperation Mean", average_coop))
 
 boxplot(cooperation ~ condition, data = Guyer,
         main = "Distribution of Cooperation vs Condition",
         xlab = "Condition",
         ylab = "Cooperation")
 
-# Some new comment
-
-# The above comment is important for the following reason ...
-
-# Some important changes need to be made here.
+# Some Change
