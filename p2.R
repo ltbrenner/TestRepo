17b1c583cdff72dd69063a520d86d5981cd426a3
ltbrenner <153870418+ltbrenner@users.noreply.github.com>
2024-05-24 14:09:25 -0700

    Automatic commit generated by codereview web-app.

--- p2.R
+++ p2.R
@@ -1,6 +1,6 @@
-vals_100 = sample(1:10, 100, replace=TRUE)
-mean = mean(vals_100)
-print(paste("Mean of 100: ", mean))
+vals_500 = sample(1:10, 500, replace=TRUE)
+mean = mean(vals_500)
+print(paste("Mean of 500: ", mean))
 
 vals_1000 = sample(1:10, 1000, replace=TRUE)
 mean = mean(vals_1000)
